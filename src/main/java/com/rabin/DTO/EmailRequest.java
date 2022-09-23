b3370ff6233dd81d6fe84922cccc12dccb24e0fe
= <=>
2022-09-23 05:15:30 -0800

    Sending email through microservice

---
+++ src/main/java/com/rabin/DTO/EmailRequest.java
@@ -0,0 +1,15 @@
+package com.rabin.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class EmailRequest {
+
+    private String toEmail;
+    private String subject;
+    private String messageBody;
+
+}
